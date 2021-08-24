8bab3465adbd99319211a04d4a23513baf7d5ed5
ethan.mcneal <ethan.mcneal@gmail.com>
2021-08-24 16:47:14 -0600

    validations

--- spec/models/article_spec.rb
+++ spec/models/article_spec.rb
@@ -1,8 +1,16 @@
 require 'rails_helper'
 
 RSpec.describe Article, type: :model do
-  it "tests article object" do
-    article = create(:article)
-    expect(article.title).to eq("Sample Article")
+  describe '#validations' do 
+  it "tests that factory is valid" do
+    let(:article) { create(:article) }
+    expect(article).to be_valid #article.valid? === true
+  end
+
+    it 'has an invalid title' do
+      article.title = ''
+      expect(article).not_to be_valid #article.valid? === true
+      expect(article.errors[:title]). to include("can't be blank")
+    end
   end
 end
