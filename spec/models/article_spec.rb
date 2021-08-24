35726a7d375539dd18c16202b84b670be265515d
ethan.mcneal <ethan.mcneal@gmail.com>
2021-08-24 16:50:16 -0600

    validation test now works

--- spec/models/article_spec.rb
+++ spec/models/article_spec.rb
@@ -2,8 +2,8 @@ require 'rails_helper'
 
 RSpec.describe Article, type: :model do
   describe '#validations' do 
+  let(:article) { build(:article) }
   it "tests that factory is valid" do
-    let(:article) { create(:article) }
     expect(article).to be_valid #article.valid? === true
   end
 
