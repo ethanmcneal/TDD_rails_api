2485fe36058d0b5a80041e033ef00d8fe70f7eba
ethan.mcneal <ethan.mcneal@gmail.com>
2021-08-24 16:36:17 -0600

    rspec test set up with factory bot

--- spec/models/article_spec.rb
+++ spec/models/article_spec.rb
@@ -1,5 +1,8 @@
 require 'rails_helper'
 
 RSpec.describe Article, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "tests article object" do
+    article = create(:article)
+    expect(article.title).to eq("Sample Article")
+  end
 end
